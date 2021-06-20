8542194a15368bbdf8785f0d93f0cfc37ed7f265
danghuutoan <danghuutoan2002@gmail.com>
2021-06-20 08:13:54 +0700

    adding book and page table, adding database migration

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://root:root@db:5432/app"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Book(db.Model):
+    __tablename__ = "books"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    author = db.Column(db.String)
+    title = db.Column(db.String)
+
+
+class Page(db.Model):
+    __tablename__ = "pages"
+    id = db.Column(db.Integer, primary_key=True)
+    index = db.Column(db.Integer)
+    content = db.Column(db.String)
+    image_path = db.Column(db.String)
 
 
 @app.route("/")
