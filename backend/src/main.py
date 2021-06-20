5211b8fa7860899b646dae75e970694791b20488
danghuutoan <danghuutoan2002@gmail.com>
2021-06-20 21:56:28 +0700

    adding basic templates

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add(
+        "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+    )
+    response.headers.add(
+        "Access-Control-Allow-Methods", "GET,PATCH,POST,DELETE,OPTIONS"
+    )
+    return response
+
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://root:root@db:5432/app"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -24,7 +40,10 @@ class Page(db.Model):
     image_path = db.Column(db.String)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route("/books", methods=["GET"])
+def create_book():
+    return render_template("home.html")
 
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
